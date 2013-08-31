345ba1a1cf79d83ae3a43198f111392d4bae566a
Pablo SEMINARIO <pablo@seminar.io>
2013-08-31 02:22:41 +0200

    Added --user-id option to import_bookmarks command

--- minne/links/management/commands/import_bookmarks.py
+++ minne/links/management/commands/import_bookmarks.py
@@ -1,4 +1,7 @@
 import datetime
+from optparse import make_option
+
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 from minne.links.models import Link
 from bs4 import BeautifulSoup
@@ -7,12 +10,20 @@ from bs4 import BeautifulSoup
 class Command(BaseCommand):
     args = '<bookmark_file>'
     help = 'Import a bookmarks file'
+    option_list = BaseCommand.option_list + (
+        make_option('--user-id',
+            action='store',
+            type='int',
+            dest='user_id',
+            help='Set owner of imported links'),
+        )
 
     def handle(self, *args, **options):
         file_path = args[0]
         file = open(file_path, 'rb')
         self.stdout.write('=> Importing %s...' % file_path.split('/')[-1])
         soup = BeautifulSoup(file.read())
+        user = User.objects.get(id=options['user_id'])
 
         for td in soup.find_all('dt')[::-1]:
             self.stdout.write(td.a.get('href'))
@@ -21,6 +32,7 @@ class Command(BaseCommand):
             link.url = td.a.get('href')
             link.added = datetime.datetime.fromtimestamp(int(td.a.get('add_date')))
             link.tags = td.a.get('tags').replace(',', ' ')
+            link.user = user
             link.save()
 
         self.stdout.write('Successfully import all bookmarks!')
